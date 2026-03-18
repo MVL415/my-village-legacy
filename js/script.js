d944eb40f144564dff92ebe2e37d9935e61f8be1
MVL415 <14sailed.genome@icloud.com>
2026-03-18 15:36:08 -0700

    Remove menu toggle functionality from script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-const toggle = document.getElementById("menu-toggle");
-const nav = document.getElementById("nav-menu");
-
-if(toggle && nav){
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("show");
-  });
-}
-
 function openLightbox(img){
   const lightbox = document.getElementById("lightbox");
   const lightboxImg = document.getElementById("lightbox-img");
@@ -23,4 +14,3 @@ function closeLightbox(){
     lightbox.style.display = "none";
   }
 }
-
