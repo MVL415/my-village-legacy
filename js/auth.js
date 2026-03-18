39adaf3a4e52a93ae3229103d1ec72e763b66be4
MVL415 <14sailed.genome@icloud.com>
2026-03-18 13:42:50 -0700

    login fix

---
+++ js/auth.js
@@ -0,0 +1,39 @@
+const auth = firebase.auth();
+
+function signUp() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      document.getElementById("user-status").innerText = "Account created!";
+    })
+    .catch(err => alert(err.message));
+}
+
+function logIn() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      document.getElementById("user-status").innerText = "Logged in!";
+    })
+    .catch(err => alert(err.message));
+}
+
+function logOut() {
+  auth.signOut().then(() => {
+    document.getElementById("user-status").innerText = "Logged out!";
+  });
+}
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    document.getElementById("user-status").innerText =
+      "Welcome, " + user.email;
+  } else {
+    document.getElementById("user-status").innerText =
+      "Not logged in";
+  }
+});
---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyXXXXAIzaSyAU4jYF4RvMgKpcXZj-dm-s7OoqE3m0f7I",
+  authDomain: "my-village-legacy.firebaseapp.com",
+  projectId: "my-village-legacy",
+  storageBucket: "my-village-legacy.firebasestorage.app",
+  messagingSenderId: "278218988505",
+  appId: "1:278218988505:web:9035b76468d5f3a8ed17c8",
+  measurementId: "G-VBZRF1LJTL"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
