1c0719a53e6a6fcfd97636fe0bfde14f92e8a9db
MVL415 <14sailed.genome@icloud.com>
2026-03-20 16:32:43 -0700

    read with me

--- js/script.js
+++ js/script.js
@@ -30,3 +30,34 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
+
+function openBookModal(title, author, img, review, question, bookId) {
+
+  document.getElementById("book-modal").style.display = "block";
+
+  document.getElementById("modal-title").innerText = title;
+  document.getElementById("modal-author").innerText = author;
+  document.getElementById("modal-img").src = img;
+  document.getElementById("modal-review").innerText = review;
+  document.getElementById("modal-question").innerText = question;
+
+  // Load comments (Giscus)
+  const comments = document.getElementById("comments-section");
+  comments.innerHTML = `
+    <script src="https://giscus.app/client.js"
+      data-repo="MVL415/mvl-comments"
+      data-repo-id="R_kgDORqDULQ"
+      data-category="General"
+      data-category-id="YOUR_CATEGORY_ID"
+      data-mapping="specific"
+      data-term="${bookId}"
+      data-theme="light"
+      crossorigin="anonymous"
+      async>
+    </script>
+  `;
+}
+
+function closeBookModal() {
+  document.getElementById("book-modal").style.display = "none";
+}
