4c8a62012cdad598173226abaff82c19ad4cee45
MVL415 <14sailed.genome@icloud.com>
2026-03-23 14:37:53 -0700

    nav update

--- js/nav.js
+++ js/nav.js
@@ -10,3 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
+
+document.querySelectorAll(".nav-item > a").forEach(item => {
+  item.addEventListener("click", function (e) {
+    const dropdown = this.nextElementSibling;
+
+    if (dropdown && dropdown.classList.contains("dropdown")) {
+      e.preventDefault();
+      dropdown.style.display =
+        dropdown.style.display === "block" ? "none" : "block";
+    }
+  });
+});
