b0e92ab9678f7c764abee45af72b957355d9d2dc
MVL415 <14sailed.genome@icloud.com>
2026-03-24 22:41:13 -0700

    new styles css

--- js/script.js
+++ js/script.js
@@ -617,3 +617,15 @@ thumbs.forEach(img => {
 
   });
 });
+
+const faders = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+faders.forEach(el => observer.observe(el));
